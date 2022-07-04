621456b9214bdcb71ac2debc3e7b700d4b604e0b
dougcaval <dougcaval.reis@gmail.com>
2022-07-04 13:17:03 -0300

    deploy test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ANOTHER DAY
+          The Writer
         </p>
         <a
           className="App-link"
